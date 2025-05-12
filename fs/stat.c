e73ae375855a0f23e0c2600872d16606218829a3
TheDoctor12 <jkaskabas95@gmail.com>
2025-05-12 14:40:23 +0300

    Revert "drivers: Import rifsxd/KernelSU-Next@c20fe6c"
    This reverts commit 3ed245a8db28fa08459bab3aef0a9a7b38efc266.

--- fs/stat.c
+++ fs/stat.c
@@ -149,8 +149,7 @@ int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 EXPORT_SYMBOL(vfs_statx_fd);
 
 #ifdef CONFIG_KSU
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user,
-			   int *flags);
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
 #endif
 
 /**
@@ -174,11 +173,9 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
-
 #ifdef CONFIG_KSU
 	ksu_handle_stat(&dfd, &filename, &flags);
 #endif
-
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
 		return -EINVAL;
